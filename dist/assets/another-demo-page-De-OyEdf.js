f3eabbdd6f963f386f4ed0bcf569a70bc8920f6f
Woyken <13166802+Woyken@users.noreply.github.com>
2025-10-20 13:39:38 +0300

    commit compiled output

---
+++ dist/assets/another-demo-page-De-OyEdf.js
@@ -0,0 +1,1 @@
+import{t as o}from"./main-DS4Ga6Bg.js";import"../entryPoint.js";var t=o('<div>Hello "/another-demo-page"!');function n(){return t()}export{n as component};
--- dist/assets/another-demo-page-mpMqKLQa.js
+++
@@ -1,1 +0,0 @@
-import{t as o}from"./main-B5Wc8BTE.js";import"../entryPoint.js";var t=o('<div>Hello "/another-demo-page"!');function n(){return t()}export{n as component};
