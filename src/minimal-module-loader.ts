1ec24d0516ac118fc33ab1d021fa4c9dd34a3d54
Woyken <13166802+Woyken@users.noreply.github.com>
2025-11-04 16:46:37 +0200

    Update module loader to expose loader function

--- src/minimal-module-loader.ts
+++ src/minimal-module-loader.ts
@@ -34,6 +34,7 @@ declare global {
 	interface Window {
 		createModuleLoader: (baseUrl?: string) => ModuleLoader;
 		__hmr_cleanups?: (() => void)[];
+		loadPageFromCdn: () => void;
 	}
 }
 
@@ -917,19 +918,23 @@ Object.keys(${tempVarName}).forEach(key => {
 	};
 };
 
-// Initialize the module loader
-document.body.innerHTML = "";
-document.head.innerHTML = "";
-document.body.appendChild(document.createElement("div")).id = "app";
+import { version as packageJsonVersion } from "../package.json" with {
+	type: "json",
+};
 
-import packageJson from "../package.json" with { type: "json" };
+window.loadPageFromCdn = () => {
+	// Initialize the module loader
+	document.body.innerHTML = "";
+	document.head.innerHTML = "";
+	document.body.appendChild(document.createElement("div")).id = "app";
 
-const loader = window.createModuleLoader(
-	`https://cdn.jsdelivr.net/gh/Woyken/project-cactus-byte@${packageJson.version}/`,
-);
-loader.loadScript(
-	`https://cdn.jsdelivr.net/gh/Woyken/project-cactus-byte@${packageJson.version}/entryPoint.js`,
-);
+	const loader = window.createModuleLoader(
+		`https://cdn.jsdelivr.net/gh/Woyken/project-cactus-byte@${packageJsonVersion}/`,
+	);
+	loader.loadScript(
+		`https://cdn.jsdelivr.net/gh/Woyken/project-cactus-byte@${packageJsonVersion}/entryPoint.js`,
+	);
 
-// const loader = window.createModuleLoader('http://localhost:3000/');
-// loader.loadScript('http://localhost:3000/src/entryPoint.ts');
+	// const loader = window.createModuleLoader('http://localhost:3000/');
+	// loader.loadScript('http://localhost:3000/src/entryPoint.ts');
+};
