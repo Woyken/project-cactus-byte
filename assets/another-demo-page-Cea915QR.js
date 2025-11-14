215eef36061d19f77fa4faf13630b648a1156c4f
Woyken <Woyken@users.noreply.github.com>
2025-11-14 15:42:44 +0000

    deploy: 1f0d61ba9505d22412172decfb3a49fd825ec669

---
+++ assets/another-demo-page-Cea915QR.js
@@ -0,0 +1,1 @@
+import{t as o}from"./main-BqNCW6Oc.js";import"../entryPoint.js";import"./index-o-WvA1Gw.js";var t=o('<div>Hello "/another-demo-page"!');function r(){return t()}export{r as component};
---
+++ assets/lazyModule-BG3TruT5.js
@@ -0,0 +1,1 @@
+const t=()=>console.log("test");export{t as test};
