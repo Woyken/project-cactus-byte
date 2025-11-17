57993afb7291bd420dbf209215ade3be64a136c6
Woyken <Woyken@users.noreply.github.com>
2025-11-17 18:53:15 +0000

    deploy: 90862fbeebf7658d676e22a67eb12183c8cb771b

---
+++ assets/another-demo-page-CH3v4QxM.js
@@ -0,0 +1,1 @@
+import{t as o}from"./main-BO6ymEz6.js";import"../entryPoint.js";import"./index-o-WvA1Gw.js";var t=o('<div>Hello "/another-demo-page"!');function r(){return t()}export{r as component};
